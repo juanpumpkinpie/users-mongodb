6ed7b992291a412f1a0ca8c634e799f9f7576e6d
juanpumpkinpie <pumpkinpiejuan@gmail.com>
2021-01-05 23:24:06 +0100

    update 8: Index.js connection mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const port = 3080;
 const app = new express();
+const User = require("./models/users.js");
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -14,17 +15,20 @@ app.listen(port, () => {
   console.log(`Server is listening in the port ${port}`);
 });
 
-mongoose.connect("mongodb://localhost/my_database", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://juan-admin:Vimeo2020@cluster0.k81xt.mongodb.net/users?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"));
 });
 
-app.post("/post/store", (req, res) => {
-  console.log(req.body);
+app.post("/post/store", async (req, res) => {
+  await User.create(req.body);
   res.redirect("/");
 });
 
